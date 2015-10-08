024ee9ea7cd7c7a9ae3e264b67b804b4a4657c2e
florije1988 <florije1988@gmail.com>
2015-10-08 10:09:13 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static final int MERCURY_YEAR = 88;
+    public static final int VENUS_YEAR = 225;
+    public static final int JUPITER_YEAR = 4380;
+    public static final int SATURE_YEAR = 11767;
+
+    public static final double MERCURY_GRAVITY = 0.4;
+    public static final double VENUS_GRAVITY = 0.9;
+    public static final double JUPITER_GRAVITY = 2.5;
+    public static final double SATURE_GRAVITY = 1.1;
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
